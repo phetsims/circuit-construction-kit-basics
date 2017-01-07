12fd57ab7df9dc27ad3a64857beaccd848625a75
zepumph <michael.kauzmann@colorado.edu>
2017-01-07 14:15:53 -0700

    lint fixes for lint-everything,  https://github.com/phetsims/tasks/issues/747

--- js/lab/LabScreen.js
+++ js/lab/LabScreen.js
@@ -26,7 +26,6 @@ define( function( require ) {
   var WireNode = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/common/view/WireNode' );
   var CustomLightBulbNode = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/common/view/CustomLightBulbNode' );
   var Property = require( 'AXON/Property' );
-  var Color = require( 'SCENERY/util/Color' );
 
   // images
   var batteryImage = require( 'mipmap!CIRCUIT_CONSTRUCTION_KIT_COMMON/battery.png' );
